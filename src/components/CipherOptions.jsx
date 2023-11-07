eb35450a8cce63481b1dcee178a965cd1ae301d8
piru72 <parvezdirom2000@gmail.com>
2023-11-07 12:50:34 +0600

    Added responsiveness to buttons

--- src/components/CipherOptions.jsx
+++ src/components/CipherOptions.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Button, ButtonGroup  ,Box } from '@chakra-ui/react'
 function CipherOptions({ onButtonClick }) {
   return (
-    <div>
-      <ButtonGroup gap='4'>
+    <Box mt={5}>
+      <ButtonGroup gap='4' display={'flex'} flexWrap={'wrap'} >
       <Button colorScheme='blue' onClick={() => onButtonClick('monoalphabetic')}>Monoalphabetic Cipher</Button>
       <Button colorScheme='blue' onClick={() => onButtonClick('polyalphabetic')}>Polyalphabetic Cipher</Button>
       <Button colorScheme='blue' onClick={() => onButtonClick('rowtransposition')}>Row Transposition Cipher</Button>
       <Button colorScheme='blue' onClick={() => onButtonClick('columntransposition')}>Column Transposition Cipher</Button>
       </ButtonGroup>
-    </div>
+    </Box>
   );
 }
 
