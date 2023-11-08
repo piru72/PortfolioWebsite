848dfc49cabc0afe36fe4bff5474ee0d7b1b00f2
piru72 <parvezdirom2000@gmail.com>
2023-11-08 09:58:35 +0600

    Nav bar and footer fixed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,7 +14,6 @@ import {
     useColorModeValue,
     Stack,
     useColorMode,
-    Center,
     IconButton,
     HStack
 
@@ -28,7 +27,7 @@ import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons'
 interface Props {
     children: React.ReactNode
 }
-const Links = ['Dashboard', 'Projects', 'Team']
+const Links = ['Ciphers', 'Blog']
 
 
 const NavLink = (props: Props) => {
@@ -50,14 +49,14 @@ const NavLink = (props: Props) => {
     )
 }
 
-export default function Nav() {
+export default function Simple() {
     const { colorMode, toggleColorMode } = useColorMode()
     const { isOpen, onOpen, onClose } = useDisclosure()
+
     return (
         <>
             <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
                 <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-
                     <IconButton
                         size={'md'}
                         icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
@@ -65,59 +64,100 @@ export default function Nav() {
                         display={{ md: 'none' }}
                         onClick={isOpen ? onClose : onOpen}
                     />
-
                     <HStack spacing={8} alignItems={'center'}>
-                        <Box>Logo</Box>
+                        <Box>Algo Applier </Box>
                         <HStack as={'nav'} spacing={4} display={{ base: 'none', md: 'flex' }}>
                             {Links.map((link) => (
                                 <NavLink key={link}>{link}</NavLink>
                             ))}
                         </HStack>
                     </HStack>
-
-
-
                     <Flex alignItems={'center'}>
-                        <Stack direction={'row'} spacing={7}>
-                            <Button onClick={toggleColorMode}>
-                                {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
-                            </Button>
-
-                            <Menu>
-                                <MenuButton
-                                    as={Button}
-                                    rounded={'full'}
-                                    variant={'link'}
-                                    cursor={'pointer'}
-                                    minW={0}>
-                                    <Avatar
-                                        size={'sm'}
-                                        src={'https://avatars.dicebear.com/api/male/username.svg'}
-                                    />
-                                </MenuButton>
-                                <MenuList alignItems={'center'}>
-                                    <br />
-                                    <Center>
-                                        <Avatar
-                                            size={'2xl'}
-                                            src={'https://avatars.dicebear.com/api/male/username.svg'}
-                                        />
-                                    </Center>
-                                    <br />
-                                    <Center>
-                                        <p>Username</p>
-                                    </Center>
-                                    <br />
-                                    <MenuDivider />
-                                    <MenuItem>Your Servers</MenuItem>
-                                    <MenuItem>Account Settings</MenuItem>
-                                    <MenuItem>Logout</MenuItem>
-                                </MenuList>
-                            </Menu>
+                    <Stack direction={'row'} spacing={7}>
+                        <Button onClick={toggleColorMode}>
+                            {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+                        </Button>
+                        <Menu>
+                            <MenuButton
+                                as={Button}
+                                rounded={'full'}
+                                variant={'link'}
+                                cursor={'pointer'}
+                                minW={0}>
+                                <Avatar
+                                    size={'sm'}
+                                    src={
+                                        'https://avatars.githubusercontent.com/u/63257806?s=96&v=4'
+                                    }
+                                />
+                            </MenuButton>
+                            <MenuList>
+                                <MenuItem>Link 1</MenuItem>
+                                <MenuItem>Link 2</MenuItem>
+                                <MenuDivider />
+                                <MenuItem>Link 3</MenuItem>
+                            </MenuList>
+                        </Menu>
                         </Stack>
                     </Flex>
                 </Flex>
+
+                {isOpen ? (
+                    <Box pb={4} display={{ md: 'none' }}>
+                        <Stack as={'nav'} spacing={4}>
+                            {Links.map((link) => (
+                                <NavLink key={link}>{link}</NavLink>
+                            ))}
+                        </Stack>
+                    </Box>
+                ) : null}
             </Box>
+
         </>
     )
 }
+
+
+
+//                     <Flex alignItems={'center'}>
+//                         <Stack direction={'row'} spacing={7}>
+//                             
+
+//                             <Menu>
+//                                 <MenuButton
+//                                     as={Button}
+//                                     rounded={'full'}
+//                                     variant={'link'}
+//                                     cursor={'pointer'}
+//                                     minW={0}>
+//                                     <Avatar
+//                                         size={'sm'}
+//                                         src={'https://avatars.dicebear.com/api/male/username.svg'}
+//                                     />
+//                                 </MenuButton>
+//                                 <MenuList alignItems={'center'}>
+//                                     <br />
+//                                     <Center>
+//                                         <Avatar
+//                                             size={'2xl'}
+//                                             src={'https://avatars.dicebear.com/api/male/username.svg'}
+//                                         />
+//                                     </Center>
+//                                     <br />
+//                                     <Center>
+//                                         <p>Username</p>
+//                                     </Center>
+//                                     <br />
+//                                     <MenuDivider />
+//                                     <MenuItem>Your Servers</MenuItem>
+//                                     <MenuItem>Account Settings</MenuItem>
+//                                     <MenuItem>Logout</MenuItem>
+//                                 </MenuList>
+//                             </Menu>
+//                         </Stack>
+//                     </Flex>
+//                 </Flex>
+//             </Box>
+//         </>
+//     )
+// }
