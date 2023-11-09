47ed3f188240bd9e6051b91f9acfca80cb7159f5
piru72 <parvezdirom2000@gmail.com>
2023-11-09 09:32:06 +0600

    Added markdown parser

--- src/components/Monoalphabetic.tsx
+++ src/components/Monoalphabetic.tsx
@@ -1,14 +1,18 @@
 
 import { Box } from '@chakra-ui/react'
-import { Heading, Text } from '@chakra-ui/react'
+import { Heading } from '@chakra-ui/react'
 import { Container } from '@chakra-ui/react'
 import { useState } from 'react';
 import { applyMonoalphabetic } from '../controllers/Ciphers.ts';
 import CipherInputOutput from './CipherInputOutput.tsx';
+import ReactMarkdown from 'react-markdown';
+import { getMonoDescription } from '../controllers/ContentProvider.ts';
+
 export default function Monoalphabetic() {
 
     const [inputValue, setInputValue] = useState('');
     const [outputValue, setOutputValue] = useState('');
+    const markdownContent = getMonoDescription();
 
     const handleCipherInput = () => {
         const cipheredText = applyMonoalphabetic(inputValue);
@@ -42,19 +46,9 @@ export default function Monoalphabetic() {
 
             <Container maxW="container.xl" centerContent mb={10} left={0}>
                 <Box maxW='50rem' className="content">
-                    <Heading mb={4}>Mono Alphabetic Substitution</Heading>
-                    <Text fontSize='xl'>
-                        In Mono-alphabetic Substitution, each of the symbols in the plaintext, say, the 26 letters for simplicity, map onto some other letter. For example,
-                    </Text>
-                    <p>
-                        <strong>Plaintext:</strong> a b c d e f g h i j k l m n o p q r s t u v w x y z
-                    </p>
-                    <p>
-                        <strong>Ciphertext:</strong> Q W E R T Y U I O P A S D F G H J K L Z X C V B N M
-                    </p>
-                    <p>
-                        The general system of symbol for symbol substitution is called a mono alphabetic substitution. 'attack' would be transformed into the cipher text 'QZZQEA'.
-                    </p>
+                    <ReactMarkdown>
+                       {markdownContent}
+                    </ReactMarkdown>
                 </Box>
 
 
---
+++ src/controllers/ContentProvider.ts
@@ -0,0 +1,18 @@
+
+const monoDescription = `
+# Mono Alphabetic Substitution
+
+In Mono-alphabetic Substitution, each of the symbols in the plaintext, say, the 26 letters for simplicity, map onto some other letter. For example,
+
+**Plaintext:** a b c d e f g h i j k l m n o p q r s t u v w x y z
+
+**Ciphertext:** Q W E R T Y U I O P A S D F G H J K L Z X C V B N M
+
+The general system of symbol for symbol substitution is called a mono alphabetic substitution. 'attack' would be transformed into the cipher text 'QZZQEA'.
+`;
+
+export function getMonoDescription() {
+    return monoDescription;
+}
+
+ 
